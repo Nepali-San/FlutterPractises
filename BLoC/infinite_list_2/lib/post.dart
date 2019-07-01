2bf0d6b4ae3e2fe3553a94308177fac4a57a1503
San Neupane <123here.comes.san@gmail.com>
2019-07-01 18:37:33 +0545

    removed some unwanted folders

--- BLoC/infinite_list_2/lib/post.dart
+++
@@ -1,7 +0,0 @@
-class Post {
-  final int id;
-  final String title;
-  final String body;
-
-  Post({this.id, this.title, this.body});
-}
--- BLoC/infinite_list_2/lib/post_event.dart
+++
@@ -1,6 +0,0 @@
-abstract class PostEvent {}
-
-class FetchEvent extends PostEvent {
-  @override
-  String toString() => "FetchEvent";
-}
